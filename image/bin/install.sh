4fd7f248a01690105139d95cc764fa315fbe7916
Michael Barton <mail@michaelbarton.me.uk>
2017-12-14 13:33:06 -0800

    Add packages required for filtering pipelines

--- image/bin/install.sh
+++ image/bin/install.sh
@@ -4,17 +4,21 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-BUILD="wget ca-certificates"
+NON_ESSENTIAL_BUILD="ca-certificates wget"
+BBTOOLS="openjdk-7-jre-headless pigz"
+
 
 # Build dependencies
 apt-get update --yes
-apt-get install --yes --no-install-recommends ${BUILD}
+apt-get install --yes --no-install-recommends ${NON_ESSENTIAL_BUILD} ${BBTOOLS}
 
 export PATH=/usr/local/bin/install:$PATH
 
 stride.sh
+bbtools.sh
 
 # Clean up dependencies
-apt-get autoremove --purge --yes ${BUILD}
+apt-get autoremove --purge --yes ${NON_ESSENTIAL_BUILD}
+apt-get install --yes --no-install-recommends ${BBTOOLS}
 apt-get clean
 rm -rf /var/lib/apt/lists/*
--- image/bin/install/stride.sh
+++ image/bin/install/stride.sh
@@ -6,7 +6,7 @@ set -o xtrace
 
 mkdir -p /usr/local/stride/bin
 
-URL="https://github.com/ythuang0522/StriDe/raw/master/StriDe_Linux64bit_v1.0.tar.gz"
+URL="https://github.com/ythuang0522/StriDe/raw/master/StriDe_Linux64bit_v${STRIDE_VERSION}.tar.gz"
 wget ${URL} --quiet --output-document - \
   | tar xzf - --directory /usr/local/stride/bin
 ln -s /usr/local/stride/bin/* /usr/local/bin
